df7e531bfd1d753c815e0c656c86083efe4a88b9
Leonard <81642556+Leonard-Theodorus@users.noreply.github.com>
2022-10-11 01:04:56 +0700

    Add files via upload
    Fixed insert, and update button

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","college_prog1");
-
-    // function select_data($conn){
-    //     $query = "SELECT * FROM mahasiswa ";
-    //     $result = mysqli_query($conn, $query);
-    //     return $result;
-    // }
-    
+    $conn = 
+    mysqli_connect
+    (
+        "localhost",
+        "root",
+        "",
+        "college_prog1"
+    );
 ?>
